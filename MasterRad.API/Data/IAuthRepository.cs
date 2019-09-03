bf22b10228dd1d8fe285a09233518d4454f9e8f6
unknown <mmilica472@gmail.com>
2019-09-04 01:00:45 +0200

    End of Section 3

---
+++ MasterRad.API/Data/IAuthRepository.cs
@@ -0,0 +1,12 @@
+using System.Threading.Tasks;
+using MasterRad.API.Models;
+
+namespace MasterRad.API.Data
+{
+    public interface IAuthRepository
+    {
+         Task<User> Register(User user, string password);
+         Task<User> Login(string username, string password);
+         Task<bool> UserExists(string username);
+    }
+}
